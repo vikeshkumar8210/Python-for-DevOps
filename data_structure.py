d1bbde315c2012437b7637d9e128027b6e443abc
vikesh <vikeshkr2ins@gmail.com>
2023-07-10 22:26:07 +0530

    3rd commit

---
+++ data_structure.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+total, used, free = shutil.disk_usage("/")
+
+print("total disk space is:",total // (2**30))
+print("used disk space is:", used // (2**30))
+print("free disk space is", free // (2**30))
